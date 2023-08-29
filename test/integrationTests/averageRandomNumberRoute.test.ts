70663043a6a47e584c346e1bda565974dffcf073
neil.townsley <neil.townsley@wipro.com>
2023-08-29 11:40:52 +0100

    fix linting issues

--- test/integrationTests/averageRandomNumberRoute.test.ts
+++ test/integrationTests/averageRandomNumberRoute.test.ts
@@ -1,12 +1,15 @@
-import {randomAverageNumberPath, randomNumberUrl} from '../../src/configurationConstants';
-import {default as request} from 'supertest';
+import {
+  randomAverageNumberPath,
+  randomNumberUrl,
+} from '../../src/configurationConstants';
+import { default as request } from 'supertest';
 import app from '../../src/app';
-import {RandomNumberSuccessfulResponse} from "../../src/service/RandomNumberResponse";
-import {randomNumberValidMockResponse} from "../mocks/randomNumberValidMockResponse";
+import { RandomNumberSuccessfulResponse } from '../../src/service/RandomNumberResponse';
+import { randomNumberValidMockResponse } from '../mocks/randomNumberValidMockResponse';
 import mockAxios from 'jest-mock-axios';
 
 const randomNumberResponse: RandomNumberSuccessfulResponse[] =
-    randomNumberValidMockResponse();
+  randomNumberValidMockResponse();
 
 describe('averageRandomNumberRoute', (): void => {
   afterEach(() => {
@@ -14,11 +17,16 @@ describe('averageRandomNumberRoute', (): void => {
   });
 
   it('When averageRandomNumberRoute is called, and external random number service is called, should return a random number list, and average of the number list is returned in response', async (): Promise<void> => {
-    mockAxios.mockResponseFor({url: randomNumberUrl}, {data: randomNumberResponse});
-    const response: request.Response = await request(app).get(randomAverageNumberPath);
+    mockAxios.mockResponseFor(
+      { url: randomNumberUrl },
+      { data: randomNumberResponse },
+    );
+    const response: request.Response = await request(app).get(
+      randomAverageNumberPath,
+    );
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.averageNumber).toEqual(23)
+    expect(response.body.averageNumber).toEqual(23);
     expect(response.body.averageNumberFixed).toEqual('23');
     expect(response.body.randomNumbers).toEqual([23]);
   });
