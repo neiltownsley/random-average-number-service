90a476fb8b0b73dfdefbaf7a9532bff678c87e89
neil.townsley <neil.townsley@wipro.com>
2023-08-29 11:34:37 +0100

    mock call to https://csrng.net/csrng/csrng.php?min=0&max=100 on integration test

--- test/integrationTests/averageRandomNumberRoute.test.ts
+++ test/integrationTests/averageRandomNumberRoute.test.ts
@@ -1,16 +1,25 @@
-import { randomAverageNumberPath } from '../../src/configurationConstants';
-import request from 'supertest';
+import {randomAverageNumberPath, randomNumberUrl} from '../../src/configurationConstants';
+import {default as request} from 'supertest';
 import app from '../../src/app';
+import {RandomNumberSuccessfulResponse} from "../../src/service/RandomNumberResponse";
+import {randomNumberValidMockResponse} from "../mocks/randomNumberValidMockResponse";
+import mockAxios from 'jest-mock-axios';
+
+const randomNumberResponse: RandomNumberSuccessfulResponse[] =
+    randomNumberValidMockResponse();
 
 describe('averageRandomNumberRoute', (): void => {
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
   it('When averageRandomNumberRoute is called, and external random number service is called, should return a random number list, and average of the number list is returned in response', async (): Promise<void> => {
-    const response: request.Response = await request(app).get(
-      randomAverageNumberPath,
-    );
+    mockAxios.mockResponseFor({url: randomNumberUrl}, {data: randomNumberResponse});
+    const response: request.Response = await request(app).get(randomAverageNumberPath);
 
     expect(response.statusCode).toBe(200);
-    expect(response.body.averageNumber).toBeDefined();
-    expect(response.body.averageNumberFixed).toBeDefined();
-    expect(response.body.randomNumbers.length).toEqual(1);
+    expect(response.body.averageNumber).toEqual(23)
+    expect(response.body.averageNumberFixed).toEqual('23');
+    expect(response.body.randomNumbers).toEqual([23]);
   });
 });
