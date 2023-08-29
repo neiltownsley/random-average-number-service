0521f2960626dac6e521eb9303bd64c8eff9dd3c
neil.townsley <neil.townsley@wipro.com>
2023-08-29 16:27:18 +0100

    update test info

--- test/unitTests/service/averageNumberCalculator.test.ts
+++ test/unitTests/service/averageNumberCalculator.test.ts
@@ -1,6 +1,6 @@
 import { averageNumberCalculator } from '../../../src/service/averageNumberCalculator';
 describe('averageNumberCalculator', (): void => {
-  it('When averageNumberCalculator is called with valid number, should return the average number', (): void => {
+  it('When averageNumberCalculator is called with valid number list, should return the average number', (): void => {
     const numberList: number[] = [2, 4, 5, 8];
     expect(averageNumberCalculator(numberList)).toEqual(4.75);
   });
